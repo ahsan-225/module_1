b66525b0cee0760f4feba1f1b9d53af3f5e8ec52
ahsan-225 <abdullahtanvir390@gmail.com>
2025-04-06 15:31:05 +0600

    Restored deleted files

---
+++ bin/assignment01.dart
@@ -0,0 +1,15 @@
+main() {
+  int x = 10;
+  int y = 20;
+
+  print("Sum = ${x + y}");
+  print("Sub = ${(x - y)}");
+  print("Multi = ${(x * y)}");
+  print("Div = ${(x / y)}");
+
+  //-------------------------------------------------------------
+
+  String z = "25";
+  int a = int.parse(z);
+  print(a);
+}
